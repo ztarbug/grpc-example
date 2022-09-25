12494965ba6efa947c5fb3dd6a4aa4df45ea6a77
ztarbug <kronprinzfriedrich@gmx.de>
2022-09-25 14:40:55 +0200

    minor changes

--- python/sampleClient.py
+++ python/sampleClient.py
@@ -1,16 +1,19 @@
 import logging
 import grpc
+import time
 
 import TrafikVerketExample_pb2
 import TrafikVerketExample_pb2_grpc
 
 def run():
-    with grpc.insecure_channel('localhost:50051') as channel:
+    with grpc.insecure_channel('127.0.0.1:50051') as channel:
         stub = TrafikVerketExample_pb2_grpc.WeatherServiceStub(channel)
         req = TrafikVerketExample_pb2.WeatherMeasurepointRequest(ids = [1,2])
         
-        resp = stub.GetWeatherMeasurepoint(req)
-        print(resp)
+        for i in range(15):
+            resp = stub.GetWeatherMeasurepoint(req)
+            print(resp)
+            time.sleep(0.8)
 
 if __name__ == '__main__':
     logging.basicConfig()
--- python/sampleServer.py
+++ python/sampleServer.py
@@ -2,6 +2,7 @@ from concurrent import futures
 import logging
 import grpc
 from google.protobuf.timestamp_pb2 import Timestamp
+import pprint
 
 import TrafikVerketExample_pb2
 import TrafikVerketExample_pb2_grpc
@@ -13,23 +14,24 @@ class WeatherServiceServicer(TrafikVerketExample_pb2_grpc.WeatherServiceServicer
 
 
     def GetWeatherMeasurepoint(self, request, context):
-            print(request)
-            context.set_code(grpc.StatusCode.OK)
-            context.set_details('Executed')
-            timestamp = Timestamp()
-            wmpList = []
-            wmp1 = TrafikVerketExample_pb2.WeatherMeasurepoint(id=1, name="station01", Observation_Air_Temperature=20.0, last_update=timestamp.GetCurrentTime())
-            wmp2 = TrafikVerketExample_pb2.WeatherMeasurepoint(id=2, name="station02", Observation_Air_Temperature=20.1, last_update=timestamp.GetCurrentTime())
-            wmpList.append(wmp1)
-            wmpList.append(wmp2)
-            
-            return TrafikVerketExample_pb2.WeatherMeasurepointList(points = wmpList)
+        print("server called")
+        pprint.pprint(request.ids)
+        context.set_code(grpc.StatusCode.OK)
+        context.set_details('Executed')
+        timestamp = Timestamp()
+        wmpList = []
+        wmp1 = TrafikVerketExample_pb2.WeatherMeasurepoint(id=1, name="station01", Observation_Air_Temperature=20.0, last_update=timestamp.GetCurrentTime())
+        wmp2 = TrafikVerketExample_pb2.WeatherMeasurepoint(id=2, name="station02", Observation_Air_Temperature=20.1, last_update=timestamp.GetCurrentTime())
+        wmpList.append(wmp1)
+        wmpList.append(wmp2)
+        
+        return TrafikVerketExample_pb2.WeatherMeasurepointList(points = wmpList)
 
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     TrafikVerketExample_pb2_grpc.add_WeatherServiceServicer_to_server(WeatherServiceServicer(), server)
-    server.add_insecure_port('[::]:50051')
+    server.add_insecure_port('127.0.0.1:50051')
     server.start()
     server.wait_for_termination()
 
